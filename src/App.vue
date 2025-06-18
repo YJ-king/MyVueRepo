ffe97448ef8933b4aaff5492666fbf8920e3e544
Lee Yeongjin <cohon@naver.com>
2025-06-18 02:52:02 +0000

    modify

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="./assets/mum.png" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You Pipelined it!" />
+      <HelloWorld msg="You Deploy it!" />
     </div>
   </header>
 
