174d128d6270f54ea9423717c0baeab2b7781064
Lee Yeongjin <cohon@naver.com>
2025-06-18 06:10:49 +0000

    lz

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/mum.png" width="500" height="500" />
+    <img alt="Vue logo" class="logo" src="./assets/mum1.png" width="500" height="500" />
 
     <div class="wrapper">
       <HelloWorld msg="You Deploy it!" />
